1de975f17eedb71fa2cfc6285980c59de94a0560
Max Vashchenko <maxvaschenko@gmail.com>
2019-06-07 10:32:48 +0300

    ~update: description fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Max Vashchenko`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Personal info and blog`,
     author: `Max Vashchenko`,
     titles: {
       blogTitle: 'Blog',

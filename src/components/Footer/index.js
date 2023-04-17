a8f6a311b245f63e9ae34d472ac3dc16d522cdb6
Max Vashchenko <maxvaschenko@gmail.com>
2023-04-17 12:14:33 +0300

    [FIX] Job place fixed. Small fixes added (#32)
    * Job place fixed. Small fixes added
    
    * package-lock.json fixed

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,11 +2,14 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import { FooterWrapper } from './styled'
 
-export const Footer = ({ copyright }) => (
-  <FooterWrapper>
-    <h2>{copyright}</h2>
-  </FooterWrapper>
-)
+export const Footer = ({ copyright }) => {
+    const year = new Date().getFullYear()
+    return (
+        <FooterWrapper>
+            <h2>{`© ${year} ${copyright}`}</h2>
+        </FooterWrapper>
+    )
+}
 
 Footer.propTypes = {
   siteTitle: PropTypes.string,
